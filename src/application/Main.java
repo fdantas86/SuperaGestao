089685953b1dfcce16c8df49e25a1ecd120fc1e4
fdantas86 <franciscodantas.oliveira@gmail.com>
2024-08-20 19:09:43 -0300

    Janela Versão Criada

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,14 +8,18 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
 			scrollPane.setFitToHeight(true);
-			scrollPane.setFitToWidth(true);			
-			Scene mainScene = new Scene(scrollPane);
+			scrollPane.setFitToWidth(true);
+			
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Supera! Gestão e Desenvolvimento");
 			primaryStage.show();
@@ -23,6 +27,10 @@ public class Main extends Application {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 	
 	public static void main(String[] args) {
